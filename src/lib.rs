7e51bd639ec862f394db56af89e3f6e4417d0892
Christopher Gagner <Christopher.Gagner@gmail.com>
2024-01-10 13:45:38 -0500

    Added the ability to handle annotation_appl.
    Added the ability to handle annotation_appl. Also started to work on comments.

--- src/lib.rs
+++ src/lib.rs
@@ -34,4 +34,49 @@ mod tests {
         println!("Result: {:?}", result);
         assert!(result.is_ok());
     }
+
+    fn check_file(entry: std::fs::DirEntry) {
+        use crate::idl::DocumentParser;
+        let contents = std::fs::read_to_string(entry.path()).unwrap();
+        let result = DocumentParser::new().parse(contents.as_str());
+        println!("Result: {:?}", result);
+        assert!(result.is_ok());
+    }
+
+    fn check_directory(entry: std::fs::DirEntry) {
+        use std::fs;
+
+        if entry.path().is_dir() {
+            let paths = fs::read_dir(entry.path()).unwrap();
+            for path in paths {
+                //println!("Name: {}", path.unwrap().path().display())
+                if let Ok(entry) = path {
+                    check_directory(entry);
+                }
+            }
+        } else {
+            if entry
+                .path()
+                .extension()
+                .unwrap_or_default()
+                .eq_ignore_ascii_case("idl")
+            {
+                println!("Found idl: {}", entry.path().to_str().unwrap());
+                check_file(entry);
+            }
+        }
+    }
+
+    #[test]
+    fn test_dds_types_test_files() {
+        use crate::idl::DocumentParser;
+        use std::fs;
+        let paths = fs::read_dir("./ext").unwrap();
+
+        for path in paths {
+            if let Ok(entry) = path {
+                check_directory(entry);
+            }
+        }
+    }
 }
